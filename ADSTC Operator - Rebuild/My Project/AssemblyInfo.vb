b9e16da592544d36a7d14094c78c35f8c98589b8
Vengelis <gaby17250@gmail.com>
2019-06-23 12:02:06 +0200

    Upload My Project Files

---
+++ ADSTC Operator - Rebuild/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Les informations générales relatives à un assembly dépendent de
+' l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
+' associées à un assembly.
+
+' Vérifiez les valeurs des attributs de l'assembly
+
+<Assembly: AssemblyTitle("ADSTC Operator - Rebuild")>
+<Assembly: AssemblyDescription("Active Directory Session Time Counter")>
+<Assembly: AssemblyCompany("VLogis Devlopment")>
+<Assembly: AssemblyProduct("ADSTC Operator - Rebuild")>
+<Assembly: AssemblyCopyright("Copyright ©  2019 VLogis Development")>
+<Assembly: AssemblyTrademark("VLogis")>
+
+<Assembly: ComVisible(False)>
+
+'Le GUID suivant est pour l'ID de la typelib si ce projet est exposé à COM
+<Assembly: Guid("ba431eb1-c437-4d68-8472-0d39ec020d68")>
+
+' Les informations de version pour un assembly se composent des quatre valeurs suivantes :
+'
+'      Version principale
+'      Version secondaire
+'      Numéro de build
+'      Révision
+'
+' Vous pouvez spécifier toutes les valeurs ou indiquer les numéros de build et de révision par défaut
+' en utilisant '*', comme indiqué ci-dessous :
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.1.10")>
